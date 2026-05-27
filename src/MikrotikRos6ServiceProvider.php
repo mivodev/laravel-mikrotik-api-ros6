da575a37e93d342a50840b8a097b2fd4e3106e76
dyzulk <66510723+dyzulk@users.noreply.github.com>
2026-05-27 21:49:47 +0700

    chore: initialize laravel wrapper package structure

---
+++ src/MikrotikRos6ServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Mivo\LaravelMikrotikRos6;
+
+use Illuminate\Support\ServiceProvider;
+
+class MikrotikRos6ServiceProvider extends ServiceProvider
+{
+    public function register(): void
+    {
+        // Register laravel-mikrotik-ros6 services
+    }
+
+    public function boot(): void
+    {
+        // Boot laravel-mikrotik-ros6 services
+    }
+}
